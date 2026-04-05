977adeef8ac1039498a0ceb4f12f79b727518d72
Ajaykumar47 <gunthapallyajaykumar47@gmail.com>
2026-04-05 22:23:24 +0530

    updates

--- Main.java
+++ Main.java
@@ -51,7 +51,7 @@ public class Main {
         payment1 =new Payment(81,"cash ","10-01-2005","UPI",true);
         payment1.displaypaymentdetails();
         payment1.statusOfTheOrder();
-       // payment1.processPaymentOnPaymentMode();
+       payment1.processPaymentOnPaymentMode();
 
       //  System.out.println("payment Status"+payment1.verifyPaymenytStatus());
 
@@ -112,7 +112,7 @@ public class Main {
 
       // Payment  payment;
        //
-        //payment = new Payment(81,"cash","10-01-2005","UPI");
+        // payment = new Payment(81,"cash","10-01-2005","UPI");
 
 
         }
